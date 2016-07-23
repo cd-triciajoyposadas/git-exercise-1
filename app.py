8ad28595ceb1d608dbf46e1cef6211f02a4216d3
joshua mantilla <joshua.emmanuel1996@gmail.com>
2016-07-24 01:07:49 +0800

    implemented api : add, list, edit, delete

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for, request
+from blueprints.band_blueprint import BAND_API
 
 app = Flask(__name__,)
 
 # Register blueprints
+app.register_blueprint(BAND_API, url_prefix = '/band')
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
