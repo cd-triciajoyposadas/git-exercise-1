a63ca2521e6e626b413b866d733ad578fcf094e8
anayatingson <anaya@codesignate.com>
2016-07-23 21:56:08 +0800

    Registered blueprint and ready endpoints

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, url_for, request, abort
+from blueprints.restaurant.restaurant_api import RESTAURANT_API
 
 app = Flask(__name__,)
 
 # Register blueprints
+app.register_blueprint(RESTAURANT_API, url_prefix= '/restaurant')
 
 if __name__ == "__main__":
 	app.run(debug=True)
+
--- blueprints/restaurant/restaurant_api.py
+++ blueprints/restaurant/restaurant_api.py
@@ -1,2 +1,24 @@
 # Restaurant API
-from flask import Flask, Blueprint, url_for, request, render_template, redirect, abort, session
+from flask import Flask, Blueprint, url_for, request, abort
+
+RESTAURANT_API = Blueprint('RESTAURANT_API', __name__)
+
+@RESTAURANT_API.route('/list/')
+def get_restaurants():
+
+	return "LIST ENDPOINT"
+
+@RESTAURANT_API.route('/add/')
+def add_restaurant():
+
+	return 'ADD ENDPOINT'
+
+@RESTAURANT_API.route('/edit/')
+def edit_restaurant():
+
+	return 'EDIT ENDPOINT'
+
+@RESTAURANT_API.route('/delete/')
+def delete_restaurant():
+
+	return 'DELETE ENDPOINT'
