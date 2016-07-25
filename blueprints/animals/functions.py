a168b9b4f7c269c42d7a6d2791166671a1dda0dd
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2016-07-25 12:55:46 +0800

    edited ferreros

--- blueprints/animals/functions.py
+++ blueprints/animals/functions.py
@@ -2,6 +2,7 @@ from flask import Flask,Blueprint,url_for,request,render_template,redirect,abort
 session
 from pymongo import MongoClient
 c = MongoClient()
+from flask import jsonify
 import json
 from bson import json_util
 
@@ -24,9 +25,12 @@ def add():
 def delete():
 	name = request.form['name']
 	
-	c.entity.animals.remove({"Name": name})
-
-	return "Data has been deleted."
+	count = c.entity.animals.find({"Name" : name}).count()
+	if count>0:
+		c.entity.animals.remove({"Name": name})
+		return "Data has been deleted."
+	else:
+		return "Data not found."
 	# return render_template('delete.html')
 
 @FUNCTIONS_API.route('/edit',methods = ["POST"])
@@ -36,14 +40,20 @@ def edit():
 	color = request.form['color']
 	age = request.form['age']
 
-	c.entity.animals.update ({"Name" : name},{"Name" : name, "Type" : atype, "Color" : color, "Age" : age})
 
-	return "Data has been edited."
+	count = c.entity.animals.find({"Name" : name}).count()
+	if count>0:
+		c.entity.animals.update ({"Name" : name},{"Name" : name, "Type" : atype, "Color" : color, "Age" : age})
+		return "Data has been edited."
+	else:
+		return "Data not found."
 	# return render_template('edit.html')
 
 @FUNCTIONS_API.route('/list')
 def list():
-	data = [i for i in c.entity.animals.find({}, {'_id': False})]
-	
-	return json.dumps(data,  sort_keys=True, indent=4, default=json_util.default)
+	# data = [i for i in c.entity.animals.find({})]
+	data = [json.loads(json_util.dumps(i)) for i in c.entity.animals.find()]
+
+	return jsonify(data)
+	# return json.dumps(data,  sort_keys=True, indent=4, default=json_util.default)
 
